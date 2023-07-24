663c406c1a68fdce983f5c9cd947134036c12a05
VincenzoDePetris <vince02@live.it>
2023-07-24 15:02:04 +0200

    add: tentativo prima consegna

---
+++ main.js
@@ -0,0 +1,14 @@
+// const askMail = prompt("inserisci qui la tua mail");
+
+// const mailAccettate = [
+//   "giorgio@gmail.com",
+//   "piero@gmail.com",
+//   "francesco@gmail.com",
+//   "genoveffa@gmail.com",
+// ];
+
+// if (mailAccettate === askMail) {
+//   alert("benvenuto");
+// } else {
+//   alert("accesso non consentito");
+// }
