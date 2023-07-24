e695191de70f70c1ee3a96fe672c54137505d1c3
VincenzoDePetris <vince02@live.it>
2023-07-24 16:41:55 +0200

    mail

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-// const askMail = prompt("inserisci qui la tua mail");
+const askMail = prompt("inserisci qui la tua mail");
 
-// const mailAccettate = [
-//   "giorgio@gmail.com",
-//   "piero@gmail.com",
-//   "francesco@gmail.com",
-//   "genoveffa@gmail.com",
-// ];
+const mailAccettate = [
+  "giorgio@gmail.com",
+  "piero@gmail.com",
+  "francesco@gmail.com",
+  "genoveffa@gmail.com",
+];
 
-// if (mailAccettate === askMail) {
-//   alert("benvenuto");
-// } else {
-//   alert("accesso non consentito");
+for (let i = 0; i < mailAccettate.length; i++) {
+  if (askMail == mailAccettate[i]) {
+    alert("benvenuto");
+  }
+}
+
+// const number = prompt("inserisci un numero da 1 a 6");
+
+// function computerNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
 
-const number = prompt("inserisci un numero da 1 a 6");
+// var random = computerNumber(1, 6);
+// console.log(random);
 
-function computerNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var random = computerNumber(1, 6);
-console.log(random);
-
-if (number > random) {
-  alert("hai vinto");
-} else if (number == random) {
-  alert("pareggio");
-} else {
-  alert("hai perso");
-}
+// if (number > random) {
+//   alert("hai vinto");
+// } else if (number == random) {
+//   alert("pareggio");
+// } else {
+//   alert("hai perso");
+// }
